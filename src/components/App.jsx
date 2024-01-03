e310811a7913b86e3a7edde3b0ee8ffac813215d
Ronald Sancho <ronalsancho@gmail.com>
2024-01-02 20:42:13 -0600

    Save text wrote into the input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(){
+
+    const [inputText, setInputText] = useState("");
+    const [items, setItems] = useState([]);
+
+    function handleChane(event){
+        const newValue = event.target.value;
+        setInputText(newValue);
+    }
+
     return (
             <div className="container">
             <div className="heading">
                 <h1>To-Do List</h1>
             </div>
             <div className="form">
-                <input type="text" />
+                <input onChange={handleChane} type="text" value={inputText} />
                 <button>
                 <span>Add</span>
                 </button>
             </div>
             <div>
                 <ul>
-                <li>A Item</li>
+                {items.map((item) => <li>{item}</li>)}
                 </ul>
             </div>
             </div>
