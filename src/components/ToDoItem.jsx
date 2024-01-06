6fcd7b9b77d7ea8bf466b1dfa6af96bbb466e443
Ronald Sancho <ronalsancho@gmail.com>
2024-01-05 20:30:01 -0600

    A check mark is added to the tasks

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToDoItem(props){   
-    return <li>{props.text}</li>;
+function ToDoItem(props){  
+
+    const [isDone, setIsDone] = useState(false);
+
+    function handleClick(){
+        setIsDone(prevValue => {
+            return !prevValue;
+        });
+    }
+
+    return (
+        <div onClick={handleClick}>
+            <li style={{textDecoration: isDone ? "line-through" : "none"}}>
+                {props.text}
+            </li>
+        </div> 
+    )
+
+
 }
 
 export default ToDoItem;
