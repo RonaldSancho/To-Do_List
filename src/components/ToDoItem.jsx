aa5407d840de96dfaea2e3306e6c01c0d3c0b6b9
Ronald Sancho <ronalsancho@gmail.com>
2024-01-06 17:16:59 -0600

    Comment some code

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import React from "react";
 
 function ToDoItem(props){  
 
-    const [isDone, setIsDone] = useState(false);
+    // const [isDone, setIsDone] = useState(false);
+
+    // function handleClick(){
+    //     setIsDone(prevValue => {
+    //         return !prevValue;
+    //     });
+    // }
 
     function handleClick(){
-        setIsDone(prevValue => {
-            return !prevValue;
-        });
+
     }
 
     return (
         <div onClick={handleClick}>
-            <li style={{textDecoration: isDone ? "line-through" : "none"}}>
+            {/* <li style={{textDecoration: isDone ? "line-through" : "none"}}> */}
+            <li>
                 {props.text}
             </li>
         </div> 
